2f2b734e5a8cefebd8fee821910a65c2c11f9e76
wxfeiang <wxfeiang@qq.com>
2023-08-25 23:30:22 +0800

    feat: 更新样式代码

--- src/subPages/subUser/PersonalIncomeTax/index.vue
+++ src/subPages/subUser/PersonalIncomeTax/index.vue
@@ -26,18 +26,50 @@ const { navOption, conunt, allList, constant } = useTax();
         :key="index"
         :title="item.title"
         :value="`${item.value}${item.unit}`"
+        :border="index != conunt.length - 1"
+        :titleStyle="item.titleStyle"
       ></u-cell>
     </u-cell-group>
   </view>
 
-  <view class="dy_body list_warp" v-for="(item, index) in allList" :key="index">
+  <view class="dy_body list_warp list_box" v-for="(item, index) in allList" :key="index">
     <!--  多个list -->
     <u-cell-group :border="false">
-      <u-cell :border="false" :title="constant.title"> </u-cell>
-      <u-cell :border="false" :title="constant.title"> </u-cell>
-      <u-cell :border="false" :title="constant.company"> </u-cell>
-      <u-cell :border="false" :title="constant.income"> </u-cell>
-      <u-cell :border="false" :title="constant.declaredTaxAmount"> </u-cell>
+      <u-cell :title="constant.title" :border="false" :titleStyle="constant.titleStyle">
+        <template #right-icon>
+          <view class="right_time">{{ item.time }}</view>
+        </template>
+      </u-cell>
+      <u-cell
+        :border="false"
+        :title="constant.smTitle + item.smTitle"
+        :titleStyle="constant.titelOthStyle"
+        class="dy_cell"
+      >
+      </u-cell>
+      <u-cell
+        :border="false"
+        :title="constant.company + item.company"
+        :isLink="true"
+        arrow-direction="right"
+        :titleStyle="constant.titelOthStyle"
+        class="dy_cell"
+      >
+      </u-cell>
+      <u-cell
+        :border="false"
+        :title="constant.income + item.income + constant.unit"
+        :titleStyle="constant.titelOthStyle"
+        class="dy_cell"
+      >
+      </u-cell>
+      <u-cell
+        :border="false"
+        :title="constant.declaredTaxAmount + item.declaredTaxAmount + constant.unit"
+        :titleStyle="constant.titelOthStyle"
+        class="dy_cell"
+      >
+      </u-cell>
     </u-cell-group>
   </view>
 </template>
@@ -50,12 +82,24 @@ body {
   padding: 0px 20px;
 }
 .list_warp {
-  margin-top: 14px;
-
+  margin-top: 12px;
   background-color: #fff;
 }
-::deep .u-cell__body {
+::v-deep .u-cell__body {
   padding-right: 0 !important;
   padding-left: 0 !important;
+}
+.list_box {
+  padding: 5px 20px 20px;
+}
+.dy_cell ::v-deep .u-cell__body {
+  padding-top: 2px !important;
+  padding-bottom: 0px !important;
+}
+.right_time {
+  color: #333;
+  padding-right: 30px;
+  font-size: 16px;
+  font-weight: 500;
 }
 </style>
--- src/subPages/subUser/PersonalIncomeTax/useTax.ts
+++ src/subPages/subUser/PersonalIncomeTax/useTax.ts
@@ -1,6 +1,6 @@
 const navOption = ref({
   leftText: "返回",
-  title: "iiiiiiii",
+  title: "收入纳税明细查询",
   rightText: "批量申诉",
   textColor: "#4985E7"
 })
@@ -10,36 +10,57 @@ const conunt = reactive(<any>[
     title: "收入合计",
     icon: "",
     value: "100000",
-    unit: "元"
+    unit: "元",
+    titleStyle: {
+      color: "#333",
+      fontWeigt: "400"
+    }
   },
   {
     title: "已申报税额合计",
     icon: "",
     value: "100000",
-    unit: "元"
+    unit: "元",
+    titleStyle: {
+      color: "#333",
+      fontWeigt: "400"
+    }
   }
 ])
 
 const allList = reactive(<any>[
   {
     time: "2023-01",
-    company: "某XXXXXXXXXXXXX",
+    smTitle: "正常工资薪金",
+    company: "西安易才人力资源有限公司",
     income: "10000",
     declaredTaxAmount: "130"
   },
   {
     time: "2023-01",
-    company: "某XXXXXXXXXXXXX",
+    smTitle: "正常工资薪金",
+    company: "西安易才人力资源有限公司",
     income: "10000",
     declaredTaxAmount: "130"
   }
 ])
 const constant = reactive({
-  title: "小类",
-  time: "时间",
-  company: "公司",
-  income: "收入",
-  declaredTaxAmount: "已申报"
+  title: "工资薪金",
+  titleStyle: {
+    color: "#333",
+    fontWeigt: "500",
+    fontSize: "16px"
+  },
+  smTitle: "所得项目小类:   ",
+  time: "2023-07",
+  company: "扣缴义务人:   ",
+  income: "收入:  ",
+  declaredTaxAmount: "已申报税额:  ",
+  unit: "元",
+  titelOthStyle: {
+    color: "#989fa5",
+    fontSize: "14px"
+  }
 })
 
 export default () => {
