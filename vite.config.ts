2ef59d3c13409301763999eebdb7419676baa1d8
wxfeiang <wxfeiang@qq.com>
2023-10-30 08:44:45 +0800

    build(custom): build  remove  console

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,14 @@ export default ({ command, mode }: ConfigEnv) => {
         },
       },
     },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    },
   };
   return result;
 };
